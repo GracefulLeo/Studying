66872385101045b013d4707ab38b8c100bf3b63b
GracefulLeo <rrty66@yandex.ru>
2017-05-12 07:52:20 +0300

    Add files via upload

---
+++ src/GameKillTheSite/DotCom.java
@@ -0,0 +1,29 @@
+package GameKillTheSite;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rrty6 on 12.05.2017.
+ */
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName (String n){
+        name = n;
+    }
+
+    public String checkYourself (String userInput){
+        String result = "мимо";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+
+        if (locationCells.isEmpty())
+        }
+    }
+}
